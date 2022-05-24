beeebd11f23b5df839d7bbd6d70d8e45cde55b8a
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-05-24 16:36:56 +0700

    update nho nho thoy . dcm sqf

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,12 +6,41 @@ import './Home.scss'
 import HomeHeader from '../../components/HomeHeader'
 import SuperVipSlider from '../../components/SuperVipSlider'
 import BackgroundIntro from '../../components/BackgroundIntro'
+import VerticalSlider from '../../components/VerticalSlider'
 
 export default function Home() {
   return (
     <div className="home">
         <BackgroundIntro/>
-        <HomeHeader />
+
+        {/* INTRO */}
+        <div className="intro">
+            <HomeHeader />
+        
+            <div className="intro-content">
+                <div className="container">
+                    <div className="row">
+                        {/* img title */}
+                        <div className="col-xl-12">
+                            <div className="intro-title-img">
+                                <img src="./images/title-intro.png" alt="" />
+                            </div>
+                        </div>
+
+                        {/* slogan text */}
+                        {/* <div className="col-xl-12">
+                            <div className="intro-title-slogan-wrap">
+                                <div className="intro-title-slogan text-gradient text-border">
+                                    Shooting game with the highest coordinates in the solar system
+                                </div>
+                            </div>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <VerticalSlider />
         {/* <SuperVipSlider /> */}
     </div>
   )
